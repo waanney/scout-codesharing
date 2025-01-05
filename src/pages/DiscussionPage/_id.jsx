e1f0808f7ec943f66d02042f4682b81b4a1bc16a
waanney <work.buiminhquan@gmail.com>
2025-01-05 21:15:47 +0700

    git

--- src/pages/DiscussionPage/_id.jsx
+++ src/pages/DiscussionPage/_id.jsx
@@ -7,7 +7,7 @@ function Board() {
   const [board, setBoard] = useState(null);
   useEffect(() => {
     // tạm thời const để test sau này dùng react router doom
-    const boardId = '677a0ae0b67142323ecfe7e3';
+    const boardId = '677a9230c4d5abf76fb2d836';
     // call API
     fetchBoardDetails_API(boardId).then(board => {
       setBoard(board);
