7263f8da889a5eaa570224e8f621ae8e306b7407
waanney <work.buiminhquan@gmail.com>
2025-01-06 22:34:40 +0700

    afjajfs

--- src/redux/apiRequest.js
+++ src/redux/apiRequest.js
@@ -1,25 +1,33 @@
 /* eslint-disable no-unused-vars */
-import axios from "axios";
-import { loginFailed, loginStart, loginSuccess, registerFailed, registerStart, registerSuccess } from "./authSlice";
+import axios from 'axios';
+import {
+  loginFailed,
+  loginStart,
+  loginSuccess,
+  registerFailed,
+  registerStart,
+  registerSuccess,
+} from './authSlice';
 
-export const loginUser = async(Users,dispatch,navigate) => {
-    dispatch(loginStart());
-    try {
-        const res = await axios.post("/v1/Auth/login",Users);
-        dispatch(loginSuccess(res.data));
-        navigate("/")
-    }catch(err) {
-        dispatch(loginFailed());
-    }
-}
-export const registerUser = async (Users,dispatch,navigate) => {
-    dispatch(registerStart());
-    try {
-        await axios.post("/v1/Auth/",Users);
-        dispatch(registerSuccess());
-        navigate("/login")
-    }catch(err) {
-        dispatch(registerFailed());
-    }
-}
+export const loginUser = async (Users, dispatch, navigate) => {
+  dispatch(loginStart());
+  try {
+    const res = await axios.post(`http://localhost:8017/v1/Auth/login`, Users);
+    dispatch(loginSuccess(res.data));
+    navigate('/');
+  } catch (err) {
+    dispatch(loginFailed());
+  }
+};
 
+// sign up
+export const registerUser = async (Users, dispatch, navigate) => {
+  dispatch(registerStart());
+  try {
+    await axios.post(`http://localhost:8017/v1/Auth/`, Users);
+    dispatch(registerSuccess());
+    navigate('/login');
+  } catch (err) {
+    dispatch(registerFailed());
+  }
+};
