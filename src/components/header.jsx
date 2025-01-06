22247e1d6e04b7de9bd6b757aff3a413af358b74
minhnguyencong1711 <minhnguyencong333@gmail.com>
2025-01-07 00:01:58 +0700

    chỉnh lại null

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export const useUser = () => {
-  const [user, setUser] = useState("null");
+  const [user, setUser] = useState(null);
 
   return { user, setUser };
 };
