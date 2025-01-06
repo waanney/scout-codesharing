5ee3427813cc2dbf49df4972e46c3fdc5c7d57ff
Peace121222 <tathanhbinh11112006@gmail.com>
2025-01-06 23:20:42 +0700

    update

--- src/redux/apiRequest.js
+++ src/redux/apiRequest.js
@@ -2,20 +2,20 @@
 import axios from "axios";
 import { loginFailed, loginStart, loginSuccess, registerFailed, registerStart, registerSuccess } from "./authSlice";
 
-export const loginUser = async(Users,dispatch,navigate) => {
+export const loginUser = async(user,dispatch,navigate) => {
     dispatch(loginStart());
     try {
-        const res = await axios.post("/v1/Auth/login",Users);
+        const res = await axios.post('http://localhost:8017/v1/Auth/login',user);
         dispatch(loginSuccess(res.data));
         navigate("/")
     }catch(err) {
         dispatch(loginFailed());
     }
 }
-export const registerUser = async (Users,dispatch,navigate) => {
+export const registerUser = async (user,dispatch,navigate) => {
     dispatch(registerStart());
     try {
-        await axios.post("/v1/Auth/",Users);
+        await axios.post('http://localhost:8017/v1/Auth/',user);
         dispatch(registerSuccess());
         navigate("/login")
     }catch(err) {
