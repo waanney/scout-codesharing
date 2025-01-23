687ab106693e9049f87b5a0a6ac10ef05d42be37
OtisHuang-new <khoihoang862006@gmail.com>
2025-01-23 18:11:49 +0700

    update: commentLine Box

--- src/pages/UsersPost/Post.jsx
+++ src/pages/UsersPost/Post.jsx
@@ -1,9 +1,9 @@
 import { Send } from 'lucide-react';
 import { Save } from 'lucide-react';
 import { Share } from 'lucide-react';
+import { MessageSquareText } from 'lucide-react';
 import HeaderForAllPages from '../../components/header.jsx';
 import FooterAllPage from '../../components/footer.jsx';
-import LineComment from '../../components/lineComment.jsx';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { fetchUserData_API } from '../../api/index.js';
@@ -21,8 +21,13 @@ function Post({ board, boardId }) {
 
   const [open, setOpen] = useState(Array(sourceCode.length).fill(false));
 
-  const SendClick = () => {
-    alert('Button clicked!');
+  const [line_content, setLineContent] = useState('');
+  const handleComment2 = async e => {
+    e.preventDefault();
+    if (!line_content.trim()) {
+      alert('Comment must have at least 1 letter.');
+      return;
+    }
   };
 
   const currentUser =
@@ -138,7 +143,9 @@ function Post({ board, boardId }) {
               <div className="card flex flex-col items-end">
                 <div>
                   <button
-                    onClick={SendClick}
+                    onClick={() => {
+                      alert('Button clicked!');
+                    }}
                     className="text-white flex flex-row items-center hover:scale-110"
                   >
                     <Share className="h-[30px] w-[30px]" />
@@ -147,7 +154,9 @@ function Post({ board, boardId }) {
                 </div>
                 <div>
                   <button
-                    onClick={SendClick}
+                    onClick={() => {
+                      alert('Button clicked!');
+                    }}
                     className="text-white flex flex-row items-center hover:scale-110"
                   >
                     <Save className="h-[30px] w-[30px]" />
@@ -164,7 +173,7 @@ function Post({ board, boardId }) {
                 {board?.title}
               </div>
             </div>
-            <div className="w-[80%] h-[60%] mx-auto px-[10px] overflow-x-auto overflow-y-auto snap-y snap-mandatory scrollbar-thumb-gray-300 scrollbar-track-[#05143c] scrollbar-thin">
+            <div className="w-[80%] h-[60%] mx-auto px-[10px] overflow-x-auto overflow-y-auto snap-y snap-mandatory scrollbar-thumb-gray-300 scrollbar-track-transparent scrollbar-thin">
               {comments.map((comment, index) => (
                 <div key={index} className="mb-4">
                   <div className="text-white text-2xl font-bold leading-9">
@@ -217,12 +226,56 @@ function Post({ board, boardId }) {
                     }}
                     className="text-end mr-[10px]"
                   >
-                    o
+                    <MessageSquareText className="opacity-30" />
                   </button>
                   <div
-                    className={`absolute w-[500px] h-[500px] bg-gray-800 right-[20px] top-[20px] z-10 transition-all transform ${open[lineNum] ? 'opacity-100 translate-y-0' : 'opacity-0 -translate-y-5'}`}
+                    className={`absolute w-[500px] h-[400px] bg-gray-800 right-[20px] top-[20px] z-10 transition-all transform ${open[lineNum] ? 'opacity-100 translate-y-0 pointer-events-auto' : 'opacity-0 -translate-y-5 pointer-events-none'}`}
                   >
-                    <LineComment />
+                    <div className="text-center text-[20px] mx-[10px]">
+                      Comment line {lineNum + 1} placeholder
+                    </div>
+                    <div className="w-[90%] h-[75%] mx-auto px-[10px] overflow-x-auto overflow-y-auto snap-y snap-mandatory scrollbar-thumb-gray-300 scrollbar-track-transparent scrollbar-thin">
+                      <p>Comment content</p>
+                      <p>Comment content</p>
+                      <p>Comment content</p>
+                      <p>Comment content</p>
+                      <p>Comment content</p>
+                      <p>Comment content</p>
+                      <p>Comment content</p>
+                      <p>Comment content</p>
+                      <p>Comment content</p>
+                      <p>Comment content</p>
+                      <p>Comment content</p>
+                      <p>Comment content</p>
+                      <p>Comment content</p>
+                      <p>Comment content</p>
+                      <p>Comment content</p>
+                      <p>Comment content</p>
+                      <p>Comment content</p>
+                      <p>Comment content</p>
+                      <p>Comment content</p>
+                      <p>Comment content</p>
+                      <p>Comment content</p>
+                    </div>
+                    <form
+                      onSubmit={handleComment2}
+                      className="flex flex-row px-[20px] py-[20px]"
+                    >
+                      <input
+                        value={line_content}
+                        onChange={e => setLineContent(e.target.value)}
+                        className="w-[90%] h-[43px] rounded-[10px] bg-[#253767] text-white text-[15px] font-normal leading-[150%] hover:drop-shadow-[0px_0px_10px_rgba(0,0,0,0.5)]
+                                  focus:border-blue-500 focus:ring focus:ring-blue-300 focus:outline-none"
+                        placeholder="  Add your comment..."
+                        type="text"
+                      />
+                      <button
+                        type="submit"
+                        className="text-white align-middle ml-[10px] rotate-45"
+                      >
+                        <Send className="h-[30px] w-[30px] hover:scale-110" />
+                      </button>
+                    </form>
                   </div>
                 </div>
               ))}
