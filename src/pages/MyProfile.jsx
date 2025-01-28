5e9c31d359c4e8211878af40f1b90ad3682c69de
minhnguyencong1711 <minhnguyencong333@gmail.com>
2025-01-28 15:49:13 +0700

    fix button

--- src/pages/MyProfile.jsx
+++ src/pages/MyProfile.jsx
@@ -385,7 +385,7 @@ function MyProfile() {
                 </div>
                 <button
                   type="submit"
-                  className="h-[40px] w-[90px] bg-white text-black rounded-[10px] font-raleway text-[16px] cursor-pointer hover:font-bold"
+                  className="cursor-pointer transition-all bg-white text-black font-bold text-[18px] px-6 py-2 mt-[4px] rounded-lg border-slate-200 border-b-[4px] hover:brightness-110 hover:-translate-y-[1px] hover:border-b-[6px] active:border-b-[2px] active:brightness-90 active:translate-y-[2px]"
                 >
                   Create
                 </button>
@@ -513,12 +513,6 @@ function MyProfile() {
                   </li>
                 ))}
               </ul>
-              <button
-                type="submit"
-                className="cursor-pointer transition-all bg-white text-black font-bold text-[18px] px-6 py-2 mt-[4px] rounded-lg border-slate-200 border-b-[4px] hover:brightness-110 hover:-translate-y-[1px] hover:border-b-[6px] active:border-b-[2px] active:brightness-90 active:translate-y-[2px]"
-              >
-                Create
-              </button>
             </div>
           </div>
         </div>
