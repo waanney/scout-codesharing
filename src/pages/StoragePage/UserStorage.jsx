008576dea86e1e45ff9c6003ad33d2073cbdddfa
OtisHuang-new <khoihoang862006@gmail.com>
2025-02-07 21:57:15 +0700

    update: paginate storage

--- src/pages/StoragePage/UserStorage.jsx
+++ src/pages/StoragePage/UserStorage.jsx
@@ -34,6 +34,22 @@ export default function UserStorage() {
     fetchSavedPosts();
   }, []);
 
+  //Phân trang
+  const [currentPage, setCurrentPage] = useState(1);
+  const postsPerPage = 4;
+
+  const indexOfFirstPost = (currentPage - 1) * postsPerPage;
+  const indexOfLastPost = indexOfFirstPost + postsPerPage - 1;
+
+  const currentPosts = savedPosts
+    ? savedPosts.slice(indexOfFirstPost, indexOfLastPost + 1)
+    : [];
+
+  const pageNumbers = [];
+  for (let i = 1; i <= Math.ceil(savedPosts.length / postsPerPage); i++) {
+    pageNumbers.push(i);
+  }
+
   return (
     <>
       <HeaderForAllPages className="sticky" />
@@ -41,27 +57,47 @@ export default function UserStorage() {
         <div className="container mx-auto">
           {' '}
           {/* Để căn giữa nội dung nếu cần */}
+          {/* Tiêu đề */}
           <h1 className="text-[48px] font-bold mt-16 mb-5 text-center text-white">
             Your saved Posts
             {/*<CommentCard />*/}
           </h1>{' '}
-          {/* Tiêu đề */}
+          {/* Phân trang */}
+          <nav className="mx-auto mb-[10px] text-[30px] ">
+            <ul className="pagination flex flex-row space-x-4 place-self-center">
+              {pageNumbers.map(number => (
+                <li
+                  key={number}
+                  className={`page-item ${currentPage === number ? 'active' : ''}`}
+                >
+                  <button
+                    onClick={() => {
+                      setCurrentPage(number);
+                    }}
+                    className={`page-link ${currentPage === number ? 'underline' : ''}`}
+                  >
+                    {number}
+                  </button>
+                </li>
+              ))}
+            </ul>
+          </nav>
           {/*grid grid-cols-1 md:grid-cols-3 gap-8*/}
-          <div className="cards grid grid-cols-2 gap-[66px]">
+          <ul className="cards grid grid-cols-2 gap-[66px]">
             {' '}
             {/* Lưới cho các card (hiện tại chỉ là khoảng trống) */}
             {/* Tạo các div rỗng để giữ chỗ cho các card, giúp background hiển thị đúng */}
-            {savedPosts.map(content => (
-              <div
+            {currentPosts.map(content => (
+              <li
                 key={content._id}
                 className="bg-navy-700 rounded-lg place-items-center"
               >
                 {' '}
                 {/* Đảm bảo mỗi post có key duy nhất */}
                 <PostCard post={content} />
-              </div>
+              </li>
             ))}
-          </div>
+          </ul>
         </div>
       </div>
       <FooterAllPage />
