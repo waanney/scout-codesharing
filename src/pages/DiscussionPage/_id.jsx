59fba47961c895d15acd840b1e97657fc470e6f3
minhnguyencong1711 <minhnguyencong333@gmail.com>
2025-02-08 22:56:47 +0700

    update discussion

--- src/pages/DiscussionPage/_id.jsx
+++ src/pages/DiscussionPage/_id.jsx
@@ -7,7 +7,7 @@ function Board() {
   const [loading, setLoading] = useState(true); // Thêm trạng thái loading
   const [error, setError] = useState(null); // Thêm trạng thái lỗi
   const [currentPage, setCurrentPage] = useState(1);
-  const postsPerPage = 10;
+  const postsPerPage = 12;
   useEffect(() => {
     const boardCollectionId = '677e53f474f256608d6044a2';
     setLoading(true); // Bắt đầu loading
