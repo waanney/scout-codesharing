9c4206c07a8c09c923634865e9bf5a14545a892f
waanney <work.buiminhquan@gmail.com>
2025-02-14 02:58:40 +0700

    try

--- src/api/index.js
+++ src/api/index.js
@@ -1,37 +1,47 @@
 // file này dùng để gọi API
 import axios from 'axios';
-import { env } from '../configs/environment.js';
 
-const API_ROOT = env.API_ROOT;
 // dùng data cho UsersPost, khi vô một bài post cụ thể thì data sẽ lấy từ đây
 export const fetchBoardDetails_API = async boardId => {
-  const response = await axios.get(`${API_ROOT}/v1/boards/${boardId}`);
+  const response = await axios.get(
+    `https://scout-backend-hluf.onrender.com/v1/boards/${boardId}`,
+  );
   return response.data;
 };
 
 // Dùng data cho Discussion, đây là 1 cái board collection chứa tất cả dữ liệu cần cho một bài post
 export const fetchBoardCollection_API = async boardCollectionId => {
-  const response = await axios.get(`${API_ROOT}/v1/page/${boardCollectionId}`);
+  const response = await axios.get(
+    `https://scout-backend-hluf.onrender.com/v1/page/${boardCollectionId}`,
+  );
   return response.data;
 };
 
 // Này để lấy thông tin của chủ sở hữu bài post
 
 export const fetchUserData_API = async userID => {
-  const response = await axios.get(`${API_ROOT}/v1/Auth/${userID}`);
+  const response = await axios.get(
+    `https://scout-backend-hluf.onrender.com/v1/Auth/${userID}`,
+  );
   return response.data;
 };
 
 export const fetchSharedPostsDetails_API = async boardIds => {
-  const response = await axios.post(`${API_ROOT}/v1/boards/details`, {
-    boardIds,
-  });
+  const response = await axios.post(
+    `https://scout-backend-hluf.onrender.com/v1/boards/details`,
+    {
+      boardIds,
+    },
+  );
   return response.data;
 };
 
 export const fetchSavedPostsDetails_API = async boardIds => {
-  const response = await axios.post(`${API_ROOT}/v1/boards/saveDetails`, {
-    boardIds,
-  });
+  const response = await axios.post(
+    `https://scout-backend-hluf.onrender.com/v1/boards/saveDetails`,
+    {
+      boardIds,
+    },
+  );
   return response.data;
 };
