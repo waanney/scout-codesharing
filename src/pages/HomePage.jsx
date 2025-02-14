8fb08536fc3b7f5e6909c7d351f732cb69edba00
waanney <work.buiminhquan@gmail.com>
2025-02-14 14:51:49 +0700

    fix

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -40,7 +40,7 @@ function HomePage() {
 
   const cardDetails = [
     {
-      img: { devTeamLogo },
+      src: { devTeamLogo },
       title: '@DC1: Devteam-4',
       description:
         'We are 4 Devmems of Fessior - GDSC - HCMUT. Especially, we are all the freshmen of Computer Science of HCMUT - OISP.',
