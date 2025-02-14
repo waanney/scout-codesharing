bad9b73c538ab751d7b7a4cc495f6e8dd4626875
waanney <work.buiminhquan@gmail.com>
2025-02-15 05:17:04 +0700

    update: animation

--- src/components/scrollTop.jsx
+++ src/components/scrollTop.jsx
@@ -6,7 +6,7 @@ export default function ScrollTop() {
       onClick={() => {
         window.scrollTo({ top: 0, behavior: 'smooth' });
       }}
-      className="fixed w-[60px] h-[60px] rounded-full bg-white right-4 bottom-7 hover:shadow-black shadow-lg"
+      className="fixed w-[60px] h-[60px] rounded-full bg-white right-4 bottom-7 hover:shadow-black shadow-lg hover:animate-bounce"
     >
       <ArrowUp className="text-black text-[60px] mx-auto size-10" />
     </button>
