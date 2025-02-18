1579d83af018fe625a6ef20ebbd87015ddcc01c0
waanney <work.buiminhquan@gmail.com>
2025-02-18 21:32:35 +0700

    create: component sharepost

--- src/pages/MyProfile.jsx
+++ src/pages/MyProfile.jsx
@@ -12,7 +12,7 @@ import ScrollTop from '~/components/scrollTop';
 import hljs from 'highlight.js';
 import '~/utils/customeStyle.css';
 import hljsLanguages from '~/utils/hljsLanguages.json';
-
+import SharedPostCo from '../components/sharePosts.jsx';
 import { env } from '~/configs/environment.js';
 //img
 import savePost from '~/assets/save.svg';
@@ -21,7 +21,7 @@ import content from '~/assets/Content.svg';
 import LoadingAnimation from '../components/loading.jsx';
 
 const API_ROOT = env.API_ROOT;
-const FE_ROOT = env.FE_ROOT;
+
 function MyProfile() {
   const { owner } = useParams();
   const { currentUserData, userId } = useUserData();
@@ -311,7 +311,6 @@ function MyProfile() {
   //biến cho sharedPosts
   const [sharedPosts, setSharedPosts] = useState([]);
   const [sharedPostAvatars, setSharedPostAvatars] = useState({});
-
   const fetchAvatarForPost = async userId => {
     try {
       const response = await axios.get(
@@ -326,7 +325,6 @@ function MyProfile() {
       return null;
     }
   };
-
   useEffect(() => {
     const fetchSharedPosts = async () => {
       const getcurrentProfile = await axios.get(`${API_ROOT}/v1/Auth/${owner}`);
@@ -337,18 +335,15 @@ function MyProfile() {
           const posts = await fetchSharedPostsDetails_API(
             currentProfile.sharedPosts,
           );
-
           const avatarPromises = posts.map(post =>
             fetchAvatarForPost(post.userId),
           );
           const avatars = await Promise.all(avatarPromises);
-
           // Tạo object avatar mapping
           const avatarMap = posts.reduce((acc, post, index) => {
             acc[post._id] = avatars[index];
             return acc;
           }, {});
-
           setSharedPostAvatars(avatarMap);
           setSharedPosts(
             posts.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt)),
@@ -364,19 +359,15 @@ function MyProfile() {
     };
     fetchSharedPosts();
   }, [owner]);
-
   if (loading) {
     <LoadingAnimation />;
   }
-
   if (error) {
     return <div className="text-red-500 p-4">Lỗi: {error}</div>;
   }
-
   if (!loading && !profileData) {
     return <div className="p-4">Không tìm thấy profile</div>;
   }
-
   return (
     <>
       <HeaderForAllPages className="sticky" />
@@ -439,7 +430,6 @@ function MyProfile() {
                   </div>
                 )}
               </div>
-
               {AvatarUrl ? (
                 <img
                   src={AvatarUrl}
@@ -456,7 +446,6 @@ function MyProfile() {
                   <circle r="71" cx="71" cy="71" fill="#D9D9D9" />
                 </svg>
               )}
-
               {isEditing && (
                 <input
                   className="flex items-center w-[250px] text-wrap"
@@ -757,100 +746,12 @@ function MyProfile() {
               </div>
             </form>
           </div>
-          {sharedPosts.map(post => (
-            <div
-              key={post._id}
-              className="cursor-pointer mt-[20px] mb-[20px] w-[90%] h-[580px] bg-black bg-opacity-50 rounded-[10px] mx-auto"
-              onClick={() =>
-                (window.location.href = `${FE_ROOT}/post/${post._id}`)
-              }
-            >
-              <div className="flex items-center space-x-1">
-                <a className="flex items-center ml-[4px] mt-[4px]">
-                  {AvatarUrl ? (
-                    <img
-                      className="aspect-square h-[30px] w-[30px] rounded-full"
-                      src={AvatarUrl}
-                      alt="Avatar"
-                    />
-                  ) : (
-                    <svg
-                      height="30"
-                      width="30"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <circle r="15" cx="15" cy="15" fill="#D9D9D9" />
-                    </svg>
-                  )}
-                  <h5 className="ml-[5px] font-Raleway font-bold text-[22px]">
-                    {profileData.username}
-                  </h5>
-                </a>
-              </div>
-              <div className="mt-[20px] w-[90%] mx-[5%] h-[85%] border-solid border-[2px] border-slate-300 rounded-[10px]">
-                <div className="flex items-center space-x-1">
-                  <a className="flex items-center ml-[4px] mt-[4px]">
-                    {sharedPostAvatars[post._id] ? (
-                      <img
-                        className="aspect-square h-[30px] w-[30px] rounded-full"
-                        src={sharedPostAvatars[post._id]}
-                        alt="Avatar"
-                      />
-                    ) : (
-                      <svg
-                        height="30"
-                        width="30"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <circle r="15" cx="15" cy="15" fill="#D9D9D9" />
-                      </svg>
-                    )}
-                    <h5 className="ml-[5px] font-Raleway font-bold text-[22px]">
-                      {post.username}
-                    </h5>
-                  </a>
-                </div>
-
-                {/* Conditional rendering to avoid errors */}
-                {post && post.content ? (
-                  <div>
-                    <h1 className="ml-[15px] text-[24px] font-bold text-center">
-                      {post.title.split(' ').slice(0, 6).join(' ')}
-                      {post.title.split(' ').length > 6 ? '...' : ''}
-                    </h1>
-                    <h2 className="ml-[30px] text-[20px] font-bold mb-[5px]">
-                      {post.description.split(' ').slice(0, 20).join(' ')}
-                      {post.description.split(' ').length > 20 ? '...' : ''}
-                    </h2>
-                    <div className="w-[95%] h-[370px] items-center bg-black bg-opacity-50 rounded-[5px] mx-[2.5%] overflow-y-auto p-4">
-                      <div className="ml-[10px] text-gray-400 text-[20px]">
-                        {post.language}
-                      </div>
-                      {post.content.split('\n').map((line, index) => (
-                        <div
-                          key={`${post._id}-${index}`}
-                          className="flex space-x-4"
-                        >
-                          <span className="text-gray-400 text-[20px]">
-                            {index + 1}.
-                          </span>
-                          <pre>
-                            <code className={`language-${post.language}`}>
-                              {line}
-                            </code>
-                          </pre>
-                        </div>
-                      ))}
-                    </div>
-                  </div>
-                ) : (
-                  <div>
-                    <p>No content available for this post.</p>
-                  </div>
-                )}
-              </div>
-            </div>
-          ))}
+          {/*shareposts*/}
+          <SharedPostCo sharedPosts={sharedPosts} 
+            sharedPostAvatars={sharedPostAvatars} 
+            AvatarUrl={AvatarUrl} 
+            profileData={profileData} 
+          />
         </div>
       </div>
       <ScrollTop />
